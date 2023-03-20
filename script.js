393cb10e43771a612241c6b54340e55eedfef371
ayesh156 <ayeshchathuranga531@gmail.com>
2023-03-20 22:11:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var bm;
+function forgotPassword() {
+    var m = document.getElementById("forgotPasswordModal");
+    bm = new bootstrap.Modal(m);
+    bm.show();
+}
+
+var bm;
+function addFeedback() {
+
+    var m = document.getElementById("addFeedback");
+    bm = new bootstrap.Modal(m);
+    bm.show();
+
+}
+
+var bm;
+function adminVerify() {
+
+    var m = document.getElementById("adminVerify");
+    bm = new bootstrap.Modal(m);
+    bm.show();
+
+}
